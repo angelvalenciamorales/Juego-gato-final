1814fb8fb46e886cb9b633035b7830b21e6e7303
angelvalenciamorales <21340736@itnogales.edu.mx>
2021-11-15 04:40:20 +0100

    Practica gato

---
+++ Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿
+namespace Gato
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDat1 = new System.Windows.Forms.TextBox();
+            this.txtDat9 = new System.Windows.Forms.TextBox();
+            this.txtDat8 = new System.Windows.Forms.TextBox();
+            this.txtDat7 = new System.Windows.Forms.TextBox();
+            this.txtDat5 = new System.Windows.Forms.TextBox();
+            this.txtDat6 = new System.Windows.Forms.TextBox();
+            this.txtDat4 = new System.Windows.Forms.TextBox();
+            this.txtDat3 = new System.Windows.Forms.TextBox();
+            this.txtDat2 = new System.Windows.Forms.TextBox();
+            this.btnLimp = new System.Windows.Forms.Button();
+            this.cboSelect = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtnam2 = new System.Windows.Forms.TextBox();
+            this.txtname1 = new System.Windows.Forms.TextBox();
+            this.lblSelect2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblGanador = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblFinal = new System.Windows.Forms.Label();
+            this.pct1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pct1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtDat1
+            // 
+            this.txtDat1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDat1.Location = new System.Drawing.Point(153, 239);
+            this.txtDat1.Name = "txtDat1";
+            this.txtDat1.Size = new System.Drawing.Size(100, 83);
+            this.txtDat1.TabIndex = 0;
+            this.txtDat1.TextChanged += new System.EventHandler(this.txtDat1_TextChanged);
+            // 
+            // txtDat9
+            // 
+            this.txtDat9.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDat9.Location = new System.Drawing.Point(430, 437);
+            this.txtDat9.Name = "txtDat9";
+            this.txtDat9.Size = new System.Drawing.Size(100, 83);
+            this.txtDat9.TabIndex = 1;
+            this.txtDat9.TextChanged += new System.EventHandler(this.txtDat9_TextChanged);
+            // 
+            // txtDat8
+            // 
+            this.txtDat8.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDat8.Location = new System.Drawing.Point(291, 437);
+            this.txtDat8.Name = "txtDat8";
+            this.txtDat8.Size = new System.Drawing.Size(100, 83);
+            this.txtDat8.TabIndex = 2;
+            this.txtDat8.TextChanged += new System.EventHandler(this.txtDat8_TextChanged);
+            // 
+            // txtDat7
+            // 
+            this.txtDat7.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDat7.Location = new System.Drawing.Point(152, 437);
+            this.txtDat7.Name = "txtDat7";
+            this.txtDat7.Size = new System.Drawing.Size(100, 83);
+            this.txtDat7.TabIndex = 3;
+            this.txtDat7.TextChanged += new System.EventHandler(this.txtDat7_TextChanged);
+            // 
+            // txtDat5
+            // 
+            this.txtDat5.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDat5.Location = new System.Drawing.Point(291, 338);
+            this.txtDat5.Name = "txtDat5";
+            this.txtDat5.Size = new System.Drawing.Size(100, 83);
+            this.txtDat5.TabIndex = 4;
+            this.txtDat5.TextChanged += new System.EventHandler(this.txtDat5_TextChanged);
+            // 
+            // txtDat6
+            // 
+            this.txtDat6.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDat6.Location = new System.Drawing.Point(430, 338);
+            this.txtDat6.Name = "txtDat6";
+            this.txtDat6.Size = new System.Drawing.Size(100, 83);
+            this.txtDat6.TabIndex = 5;
+            this.txtDat6.TextChanged += new System.EventHandler(this.txtDat6_TextChanged);
+            // 
+            // txtDat4
+            // 
+            this.txtDat4.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDat4.Location = new System.Drawing.Point(152, 338);
+            this.txtDat4.Name = "txtDat4";
+            this.txtDat4.Size = new System.Drawing.Size(100, 83);
+            this.txtDat4.TabIndex = 6;
+            this.txtDat4.TextChanged += new System.EventHandler(this.txtDat4_TextChanged);
+            // 
+            // txtDat3
+            // 
+            this.txtDat3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDat3.Location = new System.Drawing.Point(430, 239);
+            this.txtDat3.Name = "txtDat3";
+            this.txtDat3.Size = new System.Drawing.Size(100, 83);
+            this.txtDat3.TabIndex = 7;
+            this.txtDat3.TextChanged += new System.EventHandler(this.txtDat3_TextChanged);
+            // 
+            // txtDat2
+            // 
+            this.txtDat2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDat2.Location = new System.Drawing.Point(291, 239);
+            this.txtDat2.Name = "txtDat2";
+            this.txtDat2.Size = new System.Drawing.Size(100, 83);
+            this.txtDat2.TabIndex = 8;
+            this.txtDat2.TextChanged += new System.EventHandler(this.txtDat2_TextChanged);
+            // 
+            // btnLimp
+            // 
+            this.btnLimp.BackColor = System.Drawing.Color.Teal;
+            this.btnLimp.FlatAppearance.BorderSize = 0;
+            this.btnLimp.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkSlateGray;
+            this.btnLimp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimp.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimp.ForeColor = System.Drawing.Color.White;
+            this.btnLimp.Location = new System.Drawing.Point(143, 559);
+            this.btnLimp.Name = "btnLimp";
+            this.btnLimp.Size = new System.Drawing.Size(314, 99);
+            this.btnLimp.TabIndex = 9;
+            this.btnLimp.Text = "Reiniciar";
+            this.btnLimp.UseVisualStyleBackColor = false;
+            this.btnLimp.Click += new System.EventHandler(this.btnLimp_Click);
+            // 
+            // cboSelect
+            // 
+            this.cboSelect.FormattingEnabled = true;
+            this.cboSelect.Items.AddRange(new object[] {
+            "X",
+            "0"});
+            this.cboSelect.Location = new System.Drawing.Point(353, 403);
+            this.cboSelect.Name = "cboSelect";
+            this.cboSelect.Size = new System.Drawing.Size(104, 33);
+            this.cboSelect.TabIndex = 10;
+            this.cboSelect.SelectedIndexChanged += new System.EventHandler(this.cboSelect_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(52, 411);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 25);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Jugador 1";
+            // 
+            // txtnam2
+            // 
+            this.txtnam2.Location = new System.Drawing.Point(185, 475);
+            this.txtnam2.Name = "txtnam2";
+            this.txtnam2.Size = new System.Drawing.Size(125, 31);
+            this.txtnam2.TabIndex = 12;
+            // 
+            // txtname1
+            // 
+            this.txtname1.Location = new System.Drawing.Point(185, 403);
+            this.txtname1.Name = "txtname1";
+            this.txtname1.Size = new System.Drawing.Size(125, 31);
+            this.txtname1.TabIndex = 13;
+            // 
+            // lblSelect2
+            // 
+            this.lblSelect2.AutoSize = true;
+            this.lblSelect2.BackColor = System.Drawing.Color.Transparent;
+            this.lblSelect2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblSelect2.Location = new System.Drawing.Point(348, 481);
+            this.lblSelect2.Name = "lblSelect2";
+            this.lblSelect2.Size = new System.Drawing.Size(0, 25);
+            this.lblSelect2.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(52, 478);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 25);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "jugador 2";
+            // 
+            // lblGanador
+            // 
+            this.lblGanador.AutoSize = true;
+            this.lblGanador.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGanador.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblGanador.Location = new System.Drawing.Point(178, 57);
+            this.lblGanador.Name = "lblGanador";
+            this.lblGanador.Size = new System.Drawing.Size(152, 37);
+            this.lblGanador.TabIndex = 16;
+            this.lblGanador.Text = "Ganador ";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.CadetBlue;
+            this.panel1.Controls.Add(this.lblFinal);
+            this.panel1.Controls.Add(this.lblSelect2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtnam2);
+            this.panel1.Controls.Add(this.lblGanador);
+            this.panel1.Controls.Add(this.btnLimp);
+            this.panel1.Controls.Add(this.txtname1);
+            this.panel1.Controls.Add(this.cboSelect);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(587, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(570, 684);
+            this.panel1.TabIndex = 17;
+            // 
+            // lblFinal
+            // 
+            this.lblFinal.AutoSize = true;
+            this.lblFinal.Font = new System.Drawing.Font("Lucida Sans", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFinal.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblFinal.Location = new System.Drawing.Point(25, 255);
+            this.lblFinal.Name = "lblFinal";
+            this.lblFinal.Size = new System.Drawing.Size(0, 33);
+            this.lblFinal.TabIndex = 17;
+            // 
+            // pct1
+            // 
+            this.pct1.Image = global::Gato.Properties.Resources.Euh_j_ai_fait_quoi_bichette;
+            this.pct1.Location = new System.Drawing.Point(12, 12);
+            this.pct1.Name = "pct1";
+            this.pct1.Size = new System.Drawing.Size(535, 163);
+            this.pct1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pct1.TabIndex = 18;
+            this.pct1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(1157, 684);
+            this.Controls.Add(this.pct1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtDat2);
+            this.Controls.Add(this.txtDat3);
+            this.Controls.Add(this.txtDat4);
+            this.Controls.Add(this.txtDat6);
+            this.Controls.Add(this.txtDat5);
+            this.Controls.Add(this.txtDat7);
+            this.Controls.Add(this.txtDat8);
+            this.Controls.Add(this.txtDat9);
+            this.Controls.Add(this.txtDat1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pct1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDat1;
+        private System.Windows.Forms.TextBox txtDat9;
+        private System.Windows.Forms.TextBox txtDat8;
+        private System.Windows.Forms.TextBox txtDat7;
+        private System.Windows.Forms.TextBox txtDat5;
+        private System.Windows.Forms.TextBox txtDat6;
+        private System.Windows.Forms.TextBox txtDat4;
+        private System.Windows.Forms.TextBox txtDat3;
+        private System.Windows.Forms.TextBox txtDat2;
+        private System.Windows.Forms.Button btnLimp;
+        private System.Windows.Forms.ComboBox cboSelect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtnam2;
+        private System.Windows.Forms.TextBox txtname1;
+        private System.Windows.Forms.Label lblSelect2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblGanador;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblFinal;
+        private System.Windows.Forms.PictureBox pct1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Gato
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtDat1_TextChanged(object sender, EventArgs e)
+        {
+
+            if (txtDat1.Text == "0" || txtDat1.Text == "X" || txtDat1.Text == "x")
+            {
+                checarGato();
+            }
+            else
+            {
+                if (txtDat1.Text != "")
+                    MessageBox.Show("Error: Debe poner un (0) o una (x)");
+                txtDat1.Text = "";
+
+            }
+
+        }
+
+        private void txtDat2_TextChanged(object sender, EventArgs e)
+        {
+
+            if (txtDat2.Text == "0" || txtDat2.Text == "X" || txtDat2.Text == "x")
+            {
+                checarGato();
+            }
+            else
+            {
+                if (txtDat2.Text != "")
+                    MessageBox.Show("Error: Debe poner un (0) o una (x)");
+                txtDat2.Text = "";
+
+            }
+        }
+
+        private void txtDat3_TextChanged(object sender, EventArgs e)
+        {
+
+            if (txtDat3.Text == "0" || txtDat3.Text == "X" || txtDat3.Text == "x")
+            {
+                checarGato();
+            }
+            else
+            {
+                if (txtDat3.Text != "")
+                    MessageBox.Show("Error: Debe poner un (0) o una (x)");
+                txtDat3.Text = "";
+
+            }
+        }
+
+        private void txtDat4_TextChanged(object sender, EventArgs e)
+        {
+
+            if (txtDat4.Text == "0" || txtDat4.Text == "X" || txtDat4.Text == "x")
+            {
+                checarGato();
+            }
+            else
+            {
+                if (txtDat4.Text != "")
+                    MessageBox.Show("Error: Debe poner un (0) o una (x)");
+                txtDat4.Text = "";
+
+            }
+        }
+
+        private void txtDat5_TextChanged(object sender, EventArgs e)
+        {
+
+            if (txtDat5.Text == "0" || txtDat5.Text == "X" || txtDat5.Text == "x")
+            {
+                checarGato();
+            }
+            else
+            {
+                if (txtDat5.Text != "")
+                    MessageBox.Show("Error: Debe poner un (0) o una (x)");
+                txtDat5.Text = "";
+
+            }
+        }
+
+        private void txtDat6_TextChanged(object sender, EventArgs e)
+        {
+
+            if (txtDat6.Text == "0" || txtDat6.Text == "X" || txtDat6.Text == "x")
+            {
+                checarGato();
+            }
+            else
+            {
+                if (txtDat6.Text != "")
+                    MessageBox.Show("Error: Debe poner un (0) o una (x)");
+                txtDat6.Text = "";
+
+            }
+        }
+
+        private void txtDat7_TextChanged(object sender, EventArgs e)
+        {
+
+            if (txtDat7.Text == "0" || txtDat7.Text == "X" || txtDat7.Text == "x")
+            {
+                checarGato();
+            }
+            else
+            {
+                if (txtDat7.Text != "")
+                    MessageBox.Show("Error: Debe poner un (0) o una (x)");
+                txtDat7.Text = "";
+
+            }
+        }
+
+        private void txtDat8_TextChanged(object sender, EventArgs e)
+        {
+
+            if (txtDat8.Text == "0" || txtDat8.Text == "X" || txtDat8.Text == "x")
+            {
+                checarGato();
+            }
+            else
+            {
+                if (txtDat8.Text != "")
+                    MessageBox.Show("Error: Debe poner un (0) o una (x)");
+                txtDat8.Text = "";
+
+            }
+        }
+
+        private void txtDat9_TextChanged(object sender, EventArgs e)
+        {
+
+            if (txtDat9.Text == "0" || txtDat9.Text == "X" || txtDat9.Text == "x")
+            {
+                checarGato();
+            }
+            else
+            {
+                if (txtDat9.Text != "")
+                    MessageBox.Show("Error: Debe poner un (0) o una (x)");
+                txtDat9.Text = "";
+
+            }
+        }
+
+        //checar gato..
+        private void checarGato()
+        {
+            //optimizacion de  codigo...
+            String c1, c2, c3, c4, c5, c6, c7, c8, c9;
+            string j1, j2,res;
+            j1 = txtname1.Text.ToUpper();
+            j2 = txtnam2.Text.ToUpper();
+            res = cboSelect.Text.ToUpper();
+
+            c1 = txtDat1.Text.ToUpper();
+            c2 = txtDat2.Text.ToUpper();
+            c3 = txtDat3.Text.ToUpper();
+            c4 = txtDat4.Text.ToUpper();
+            c5 = txtDat5.Text.ToUpper();
+            c6 = txtDat6.Text.ToUpper();
+            c7 = txtDat7.Text.ToUpper();
+            c8 = txtDat8.Text.ToUpper();
+            c9 = txtDat9.Text.ToUpper();
+
+
+            if (c1 == "0" && c2 == "0" && c3 == "0" ||
+                c1 == "X" && c2 == "X" && c3 == "X")
+            {
+                if (c1 == "0" && res == "0"  ) lblFinal.Text = ("El ganador es (0) " + j1 + "");
+                if (c1 == "X" && lblSelect2.Text == "X") lblFinal.Text = ("El ganador es (X) " + j2 + "");
+
+                if (c1 == "0" && lblSelect2.Text == "0") lblFinal.Text = ("El ganador es (0) " + j2 + "");
+                if (c1 == "X" && res == "X") lblFinal.Text = ("El ganador es (X) " + j1 + "");
+            }
+            if (c4 == "0" && c5 == "0" && c6 == "0" ||
+                c4 == "X" && c5 == "X" && c6 == "X")
+            {
+                if (c4 == "0" && res == "0") lblFinal.Text = ("El ganador es (0) " + j1 + "");
+                if (c4 == "X" && lblSelect2.Text == "X") lblFinal.Text = ("El ganador es (X) " + j2 + "");
+
+                if (c4 == "0" && lblSelect2.Text == "0") lblFinal.Text = ("El ganador es (0) " + j2 + "");
+                if (c4 == "X" && res == "X") lblFinal.Text = ("El ganador es (X) " + j1 + "");
+            }
+
+            if (c7 == "0" && c8 == "0" && c9 == "0" ||
+                c7 == "X" && c8 == "X" && c9 == "X")
+            {
+                if (c7 == "0" && res == "0") lblFinal.Text = ("El ganador es (0) " + j1 + "");
+                if (c7 == "X" && lblSelect2.Text == "X") lblFinal.Text = ("El ganador es (X) " + j2 + "");
+
+                if (c7 == "0" && lblSelect2.Text == "0") lblFinal.Text = ("El ganador es (0) " + j2 + "");
+                if (c7 == "X" && res == "X") lblFinal.Text = ("El ganador es (X) " + j1 + "");
+            }
+
+            if (c1 == "0" && c4 == "0" && c7 == "0" ||
+                  c1 == "X" && c4 == "X" && c7 == "X")
+            {
+                if (c4 == "0" && res == "0") lblFinal.Text = ("El ganador es (0) " + j1 + "");
+                if (c4 == "X" && lblSelect2.Text == "X") lblFinal.Text = ("El ganador es (X) " + j2 + "");
+
+                if (c4 == "0" && lblSelect2.Text == "0") lblFinal.Text = ("El ganador es (0) " + j2 + "");
+                if (c4 == "X" && res == "X") lblFinal.Text = ("El ganador es (X) " + j1 + "");
+            }
+            if (c2 == "0" && c5 == "0" && c8 == "0" ||
+                 c2 == "X" && c5 == "X" && c8 == "X")
+            {
+                if (c2 == "0" && res == "0") lblFinal.Text = ("El ganador es (0) " + j1 + "");
+                if (c2 == "X" && lblSelect2.Text == "X") lblFinal.Text = ("El ganador es (X) " + j2 + "");
+
+                if (c2 == "0" && lblSelect2.Text == "0") lblFinal.Text = ("El ganador es (0) " + j2 + "");
+                if (c2 == "X" && res == "X") lblFinal.Text = ("El ganador es (X) " + j1 + "");
+            }
+            if (c3 == "0" && c6 == "0" && c9 == "0" ||
+                 c3 == "X" && c6 == "X" && c9 == "X")
+            {
+                if (c3 == "0" && res == "0") lblFinal.Text = ("El ganador es (0) " + j1 + "");
+                if (c3 == "X" && lblSelect2.Text == "X") lblFinal.Text = ("El ganador es (X) " + j2 + "");
+
+                if (c3 == "0" && lblSelect2.Text == "0") lblFinal.Text = ("El ganador es (0) " + j2 + "");
+                if (c3 == "X" && res == "X") lblFinal.Text = ("El ganador es (X) " + j1 + "");
+            }
+            if (c3 == "0" && c5 == "0" && c7 == "0" ||
+                 c3 == "X" && c5 == "X" && c7 == "X")
+            {
+                if (c3 == "0" && res == "0") lblFinal.Text = ("El ganador es (0) " + j1 + "");
+                if (c3 == "X" && lblSelect2.Text == "X") lblFinal.Text = ("El ganador es (X) " + j2 + "");
+
+                if (c3 == "0" && lblSelect2.Text == "0") lblFinal.Text = ("El ganador es (0) " + j2 + "");
+                if (c3 == "X" && res == "X") lblFinal.Text = ("El ganador es (X) " + j1 + "");
+            }
+            if (c1 == "0" && c5 == "0" && c9 == "0" ||
+                 c1 == "X" && c5 == "X" && c9 == "X")
+            {
+                if (c1 == "0" && res == "0") lblFinal.Text = ("El ganador es (0) " + j1 + "");
+                if (c1 == "X" && lblSelect2.Text == "X") lblFinal.Text = ("El ganador es (X) " + j2 + "");
+
+                if (c1 == "0" && lblSelect2.Text == "0") lblFinal.Text = ("El ganador es (0) " + j2 + "");
+                if (c1 == "X" && res == "X") lblFinal.Text = ("El ganador es (X) " + j1 + "");
+            }
+
+            ////empate
+            //if (c4 == "0" && c5 == "X" && c6 == "0" ||
+            //   c4 == "X" && c5 == "0" && c6 == "X")
+            //{
+            //    if (c4 == "0") MessageBox.Show("Es un empate");
+            //}
+            //if (c7 == "0" && c8 == "X" && c9 == "0" ||
+            //  c7 == "X" && c8 == "0" && c9 == "X")
+            //{
+            //    if (c7 == "0") MessageBox.Show("Es un empate");
+            //}
+            //if (c1 == "0" && c4 == "X" && c7 == "0" ||
+            //     c1 == "X" && c4 == "0" && c7 == "X")
+            //{
+            //    if (c1 == "0") MessageBox.Show("Es un empate");
+            //}
+            //if (c2 == "0" && c5 == "X" && c8 == "0" ||
+            //    c2 == "X" && c5 == "0" && c8 == "X")
+            //{
+            //    if (c2 == "0") MessageBox.Show("Es un empate");
+            //}
+
+            //if (c3 == "0" && c6 == "X" && c9 == "0" ||
+            //    c3 == "X" && c6 == "0" && c9 == "X")
+            //{
+            //    if (c3 == "0") MessageBox.Show("Es un empate");
+            //}
+            //if (c3 == "0" && c5 == "X" && c7 == "0" ||
+            //    c3 == "X" && c5 == "0" && c7 == "X")
+            //{
+            //    if (c3 == "0") MessageBox.Show("Es un empate");
+            //}
+
+        }
+
+        private void btnLimp_Click(object sender, EventArgs e)
+        {
+            //se limpian los texbox para generar nueva partida..
+            txtDat1.Text = "";
+            txtDat2.Text = "";
+            txtDat3.Text = "";
+            txtDat4.Text = "";
+            txtDat5.Text = "";
+            txtDat6.Text = "";
+            txtDat7.Text = "";
+            txtDat8.Text = "";
+            txtDat9.Text = "";
+            lblFinal.Text = "";
+        }
+
+        private void cboSelect_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+            if (cboSelect.Text == "0")
+            {
+
+                lblSelect2.Text = "X";
+            }
+            else
+            {
+                lblSelect2.Text = "0";
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bienvenido enter para continuar");
+
+
+        }
+    }
+}
